88bcc6081f0ca3983f9f8ab60bf8b38bf7a620d0
Your Name <your.email@example.com>
2024-02-28 07:11:44 -0800

    Removed unused script.js, extrra bundle.js

--- src/scripts.js
+++
@@ -1,9 +0,0 @@
-// Fetch environment variables from the server
-fetch('/api/env')
-  .then(response => response.json())
-  .then(env => {
-    // Use environment variables
-    console.log(env.GOOGLE_API_KEY);
-    // You can use other environment variables similarly
-  })
-  .catch(error => console.error('Error fetching environment variables:', error));
