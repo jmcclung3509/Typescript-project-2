aa06648faa94ff2d38b51a73e2a64fc8fe454eeb
Your Name <your.email@example.com>
2024-02-28 08:23:08 -0800

    Added Dotenv back

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const webpack = require('webpack')
-const dotenv = require('dotenv')
-dotenv.config();
-
+const webpack = require('webpack');
+const Dotenv = require('dotenv');
 
 module.exports = {
   mode: "production",
@@ -31,14 +29,16 @@ module.exports = {
   devtool: "eval-source-map",
   plugins: [
     new CleanWebpackPlugin(),
-    new webpack.DefinePlugin({
-        'process.env': JSON.stringify(process.env)
+    new Dotenv({
+      systemvars: true, // Load environment variables from `process.env`
     }),
-
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env), // Define process.env for client-side code
+    }),
     new HtmlWebpackPlugin({
       template: './index.html', // Path to your HTML template
       filename: 'index.html', // Output filename
       inject: 'body', // Inject scripts into the body of the HTML file
-    })
+    }),
   ],
 };
